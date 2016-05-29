da673c1bb1212339402d08ac4d662a7c17c344e1
Saurabh Dixit <saurabh.iips@gmail.com>
2016-05-29 17:29:26 +0530

    Updated

---
+++ mrmedservices/src/org/repository/Mapper/UserObjConverter.java
@@ -0,0 +1,46 @@
+package org.repository.Mapper;
+
+import org.presentation.entities.user.ReqObjUser;
+import org.repository.BObjects.UserBO;
+
+public class UserObjConverter {
+
+	private UserBO ubo;
+	private ReqObjUser reqObj;
+	
+	public void convertToBO(ReqObjUser req){
+		ubo.setActive(req.getAct());
+		ubo.setAdd(req.getAdd());
+		ubo.setAge(req.getAge());
+		ubo.setConNu(req.getCon());
+		ubo.setEmail(req.getEm());
+		ubo.setfName(req.getFn());
+		ubo.setGender(req.getGen());
+		ubo.setjDate(req.getJd());
+		ubo.setLastSeen(req.getLs());
+		ubo.setlName(req.getLn());
+		ubo.setLoginSt(req.getLst());
+		ubo.setPwd(req.getPwd());
+		ubo.setRole(req.getRo());
+		ubo.setuId(req.getuId());
+		ubo.setUserName(req.getUn());
+	}
+	
+	public void convertFromBO(UserBO uBObj){
+		reqObj.setAct(uBObj.getActive());
+		reqObj.setAdd(uBObj.getAdd());
+		reqObj.setAge(uBObj.getAge());
+		reqObj.setCon(uBObj.getConNu());
+		reqObj.setEm(uBObj.getEmail());
+		reqObj.setFn(uBObj.getfName());
+		reqObj.setGen(uBObj.getGender());
+		reqObj.setJd(uBObj.getjDate());
+		reqObj.setLn(uBObj.getlName());
+		reqObj.setLs(uBObj.getLastSeen());
+		reqObj.setLst(uBObj.getLoginSt());
+		reqObj.setPwd(uBObj.getPwd());
+		reqObj.setRo(uBObj.getPwd());
+		reqObj.setuId(uBObj.getuId());
+		reqObj.setUn(uBObj.getUserName());
+	}
+}
