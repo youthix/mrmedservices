fef25bcc96bfab59d049388f0c93a915823dc6e8
saurabh <inbox.saurabh@gmail.com>
2016-05-30 10:48:16 -0500

    update

--- mrmedservices/src/org/repository/DomainConverter/DomainObjConverter.java
+++ mrmedservices/src/org/repository/DomainConverter/DomainObjConverter.java
@@ -3,8 +3,11 @@ package org.repository.DomainConverter;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.presentation.entities.customer.ReqObjCustomer;
+import org.presentation.entities.customer.ResObjCustomer;
 import org.presentation.entities.user.ReqObjUser;
 import org.presentation.entities.user.ResObjUser;
+import org.repository.BObjects.CustomerBO;
 import org.repository.BObjects.UserBO;
 public class DomainObjConverter {
 
@@ -39,7 +42,7 @@ public class DomainObjConverter {
 		return ubo;
 	}
 	
-	private ResObjUser convertFromBO(UserBO uBObj){
+	public ResObjUser convertFromBO(UserBO uBObj){
 		resObj=new ResObjUser();
 		resObj.setAct(uBObj.getActive());
 		resObj.setAdd(uBObj.getAdd());
@@ -58,4 +61,50 @@ public class DomainObjConverter {
 		resObj.setUn(uBObj.getUserName());
 		return resObj;
 	}
+	
+	public CustomerBO convertToBO(ReqObjCustomer req){
+		CustomerBO cbo=new CustomerBO();
+		cbo.setActive(req.getAct());
+		cbo.setAdd(req.getAdd());
+		cbo.setAge(req.getAge());
+		cbo.setConNu(req.getCon());
+		cbo.setCusID(req.getcId());
+		cbo.setCusTy(req.getcTy());
+		cbo.setEmail(req.getEm());
+		cbo.setfName(req.getFn());
+		cbo.setGender(req.getGen());
+		cbo.setjDate(req.getJd());
+		cbo.setLastSeen(req.getLs());
+		cbo.setlName(req.getLn());
+		cbo.setlNo(req.getlNo());
+		cbo.setPan(req.getPan());
+		cbo.setTin(req.getTin());
+		cbo.setuId(req.getuId());
+		
+		return cbo;
+	}
+	
+	public ResObjCustomer convertFromBO(CustomerBO cBObj){
+		
+		ResObjCustomer resObjCust=new ResObjCustomer();
+		
+		resObjCust.setAct(cBObj.getActive());
+		resObjCust.setAdd(cBObj.getAdd());
+		resObjCust.setAge(cBObj.getAge());
+		resObjCust.setcId(cBObj.getCusID());
+		resObjCust.setCon(cBObj.getConNu());
+		resObjCust.setcTy(cBObj.getCusTy());
+		resObjCust.setEm(cBObj.getEmail());
+		resObjCust.setFn(cBObj.getfName());
+		resObjCust.setGen(cBObj.getGender());
+		resObjCust.setJd(cBObj.getjDate());
+		resObjCust.setLn(cBObj.getlName());
+		resObjCust.setlNo(cBObj.getlNo());
+		resObjCust.setLs(cBObj.getLastSeen());
+		resObjCust.setPan(cBObj.getPan());
+		resObjCust.setTin(cBObj.getTin());
+		resObjCust.setuId(cBObj.getuId());
+		
+		return resObjCust;
+	}	
 }
